5fba081b22e93a424d00ddedef179edbfa124960
NektariosX01 <nektarios@circlelinkhealth.com>
2021-01-17 16:02:51 +0200

    final commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,61 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = currentResult + 10
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
+}
 
-outputResult(currentResult, 'the result is');
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.table(logEntries);
+}
+
+function add() {
+    const enteredNumber = parseInt(userInput.value);
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber)
+    writeToLog('ADD', initialResult, enteredNumber, currentResult)
+
+}
+
+function subtract() {
+    const enteredNumber = parseInt(userInput.value);
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber)
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+}
+
+function multiply() {
+    const enteredNumber = parseInt(userInput.value);
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber)
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
+}
+
+function divide() {
+    const enteredNumber = parseInt(userInput.value);
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber)
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
+}
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
